133002e100df03c1e70656f16b020fff1dfc80a3
rpernischova <romana.pernischova@uzh.ch>
2014-11-22 09:20:26 +0100

    hi!!

--- AgrarAlpha_v1/src/com/gwt/server/VisualizationBarChart.java
+++ AgrarAlpha_v1/src/com/gwt/server/VisualizationBarChart.java
@@ -2,4 +2,13 @@ package com.gwt.server;
 
 /*public class VisualizationBarChart implements Visualization {
 	
+	//beispiel
+	Chart chart = new Chart()
+   .setType(Series.Type.SPLINE)
+   .setChartTitleText("Lawn Tunnels")
+   .setMarginRight(10);
+   Series series = chart.createSeries()
+   .setName("Moles per Yard")
+   .setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
+chart.addSeries(series);
 }*/
