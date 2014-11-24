5c319544ab90c830758d66bd45bebb48d4c8489e
billbos <b.bosshard@hotmail.de>
2014-11-24 20:16:02 +0100

    xvc

---
+++ AgrarAlpha_v1/src/com/gwt/client/VisualizationTable.java
@@ -0,0 +1,101 @@
+package com.gwt.client;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ */
+public class VisualizationTable  {
+
+private	String tr="<tr>";
+private	String tr2="</tr>";
+private	String th="<th>";
+private	String th2="</th>";
+private	String td="<td>";
+private	String td2="</td>";
+private HTML table;
+private String htmlString;
+ArrayList<String[]> arraylist;
+
+public VisualizationTable(ArrayList<String[]>a){
+	this.arraylist=a;
+}
+	public HTML create() {
+	
+/*  String[] sArray= {"Schweiz","2","3"};
+  String[] sArray1= {"Schweiz","12","13"};
+  String[] sArray2= {"Frankreich","22","23"};
+  String[] sArray3= {"Frankreich","32","33"};
+  String[] sArray4= {"Schweiz","2","3"};
+  String[] sArray5= {"Schweiz","12","13"};
+  String[] sArray6= {"Frankreich","22","23"};
+  String[] sArray7= {"Frankreich","32","33"};
+  arraylist.add(sArray);
+  arraylist.add(sArray1);
+  arraylist.add(sArray2);
+  arraylist.add(sArray3);
+  arraylist.add(sArray4);
+  arraylist.add(sArray5);
+  arraylist.add(sArray6);
+  arraylist.add(sArray7);
+*/
+	htmlString+="<table border=\"1\" style=\"width:100%\">";
+
+
+	
+		HTML html=new HTML();
+		htmlString+="<thead>";
+		htmlString+=tr;
+		htmlString+=th+"Country"+th2;
+		for(int a=1990;a<2012;a++){
+	    
+		htmlString+=th+String.valueOf(a)+th2;}
+		htmlString+="</tr>";
+		htmlString+="</thead>";
+		htmlString+="<tbody>";
+        String checkCountry=arraylist.get(0)[0];
+		int i=0;
+		int k=0;
+		
+		while(i<arraylist.size()-1){
+			htmlString+=tr;
+			htmlString+=td+arraylist.get(i)[0]+td2;
+			while(i<arraylist.size()-1 && checkCountry.equals(arraylist.get(k)[0])){
+				htmlString+=td+arraylist.get(i)[1]+td2;
+		      i++;
+		      if(i<arraylist.size()-2)
+		    	  k=i;
+		      
+			}
+			if(i<arraylist.size())
+			checkCountry=arraylist.get(i)[0];
+			
+			
+			htmlString+=tr2;
+		}
+		htmlString+="<tbody>";
+
+		htmlString+="</table>";
+
+		html.setHTML(htmlString);
+        return html;		
+		}
+}
