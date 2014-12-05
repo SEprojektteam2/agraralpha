e09774a6835f8a99182b08d743d894f550f4da73
Bill <Bill@Bill-PC>
2014-12-05 14:45:22 +0100

    gfh

--- AgrarAlpha_v1/src/com/gwt/client/OpenView.java
+++ AgrarAlpha_v1/src/com/gwt/client/OpenView.java
@@ -1,19 +1,41 @@
 package com.gwt.client;
 
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 // only placeholder at the moment
 public class OpenView extends Composite {
 
-	 private FlowPanel fPanel= new FlowPanel();
+	 private VerticalPanel vPanel= new  VerticalPanel();
      private Button b;	
+     private TextBox tb;
+     
      
      public OpenView(){
-    	 initWidget(this.fPanel);
+    	 initWidget(this.vPanel);
 
-    	 b= new Button("coming soon4");
-    	 fPanel.add(b);
+    	 b= new Button("Save Options");
+    	 
+    	 tb=new TextBox();
+    	 tb.setText("Open");
+    	 tb.addKeyPressHandler(new KeyPressHandler() {
+    		  @Override
+    		  public void onKeyPress(KeyPressEvent event) {
+    		    TextBox sender = (TextBox) event.getSource();
+
+    		    int keyCode = event.getNativeEvent().getKeyCode();
+
+    		    if (!(Character.isLetterOrDigit(event.getCharCode()))) {
+    		      sender.cancelKey();
+    		    }
+    		  }
+    		});
+    	 vPanel.add(b);    
+    	 vPanel.add(tb);
     	 
      }
 
