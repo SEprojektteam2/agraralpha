0f6cf9b74c52bc028e427ef5adbe5eab9faa0da4
WilliamMartini <william.martini@outlook.com>
2014-12-05 15:55:28 +0100

    bla

---
+++ AgrarAlpha_v1/src/com/gwt/client/DialogBoxSave.java
@@ -0,0 +1,60 @@
+package com.gwt.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+// only placeholder at the moment
+public class DialogBoxSave extends DialogBox {
+
+	 private VerticalPanel vPanel= new  VerticalPanel();
+     private TextBox tb;
+     
+     
+     public DialogBoxSave(){
+
+    	 
+    	 tb=new TextBox();
+    	 tb.addKeyPressHandler(new KeyPressHandler() {
+    		  @Override
+    		  public void onKeyPress(KeyPressEvent event) {
+    		    TextBox sender = (TextBox) event.getSource();
+
+    		    int keyCode = event.getNativeEvent().getKeyCode();
+
+    		    if (!(Character.isLetterOrDigit(event.getCharCode()))) {
+    		      sender.cancelKey();
+    		    }
+    		  }
+    		});
+    	setWidget(tb);
+    	 // Set the dialog box's caption.
+         setText("Invalid selection");
+
+         // Enable animation.
+         setAnimationEnabled(true);
+
+         // Enable glass background.
+         setGlassEnabled(true);
+
+         // DialogBox is a SimplePanel, so you have to set its widget property to
+         // whatever you want its contents to be.
+         Button save = new Button("Save As");
+         save.addClickHandler(new ClickHandler() {
+           public void onClick(ClickEvent event) {
+             DialogBoxSave.this.hide();
+           }
+         });
+         setWidget(save);
+       }
+    	 
+     
+
+
+}
