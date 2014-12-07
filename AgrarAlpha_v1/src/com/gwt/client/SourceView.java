892c48a64321dab60df723c870efd707a9b51e06
billbos <b.bosshard@hotmail.de>
2014-12-07 18:51:32 +0100

    Update SourceView.java

--- AgrarAlpha_v1/src/com/gwt/client/SourceView.java
+++ AgrarAlpha_v1/src/com/gwt/client/SourceView.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
+/*
+ Simple VerticalPanel with label witch contains the sources
+ */
 public class SourceView extends Composite {
 
 	private VerticalPanel vPanel= new VerticalPanel();
@@ -15,7 +17,7 @@ public class SourceView extends Composite {
 		addSource("Source: FAO. 2014. FAOSTAT. data.fao.org. (Accessed 1.9.2014)");
 
 	}
-	
+	//add source to the panel
 	public void addSource(String s){
 		label=new Label(s);
 		vPanel.add(label);
