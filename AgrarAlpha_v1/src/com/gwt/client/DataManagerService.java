6fbd5b28c00c6888f7bb4c29e9b9ef5f7b9a7f33
WilliamMartini <william.martini@outlook.com>
2014-12-08 12:49:20 +0100

    Wenn die Lippen abhanden kommen, frieren die Zähne.

--- AgrarAlpha_v1/src/com/gwt/client/DataManagerService.java
+++ AgrarAlpha_v1/src/com/gwt/client/DataManagerService.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+/**
+ * RPC
+ * 
+ * @author William Martini
+ *
+ */
 @RemoteServiceRelativePath("DataManager")
 public interface DataManagerService extends RemoteService {
 	ArrayList<String> getCountries() throws IllegalArgumentException;
--- AgrarAlpha_v1/src/com/gwt/client/SimpleRegressionServiceAsync.java
+++ AgrarAlpha_v1/src/com/gwt/client/SimpleRegressionServiceAsync.java
@@ -1,8 +1,13 @@
 package com.gwt.client;
-import java.util.ArrayList;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+/**
+ * RPC
+ * 
+ * @author William Martini
+ *
+ */
 public interface SimpleRegressionServiceAsync {
 	void getSimpleReg(double[][] data, AsyncCallback<double[]> callback) throws IllegalArgumentException;
 }
