716009db6f0a5737e818a34ced1aec28fec3d413
WilliamMartini <william.martini@outlook.com>
2014-12-08 12:05:41 +0100

    bla

--- AgrarAlpha_v1/src/com/gwt/client/VisualizationLineChart.java
+++ AgrarAlpha_v1/src/com/gwt/client/VisualizationLineChart.java
@@ -1,124 +1,123 @@
 package com.gwt.client;
+
 import java.util.ArrayList;
 import java.util.logging.Logger;
+import com.google.gwt.i18n.client.*;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.i18n.client.*;  
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-import org.moxieapps.gwt.highcharts.client.*;   
+import org.moxieapps.gwt.highcharts.client.*;
 import org.moxieapps.gwt.highcharts.client.plotOptions.*;
 
+/**
+ * @author William Martini
+ *
+ */
+public class VisualizationLineChart {
+	public static Logger log = Logger.getLogger(VisualizationLineChart.class
+			.getName());
 
-//public class VisualizationLineChart implements EntryPoint{
-public class VisualizationLineChart{
-	public static Logger log = Logger.getLogger(VisualizationLineChart.class.getName());
-	
-	
 	private double[] resultReg;
 	private double[] points;
-	
+
 	final Chart chart = new Chart();
 	ArrayList<String[]> resultData;
-	
-	
-	public Chart getLineChart(ArrayList<String[]> resultData, double[] points, double[] regressionPoints){
+
+	/**
+	 * @param resultData
+	 * @param points
+	 * @param regressionPoints
+	 * @return Chart
+	 */
+	public Chart getLineChart(ArrayList<String[]> resultData, double[] points,
+			double[] regressionPoints) {
 		this.resultReg = regressionPoints;
 		this.resultData = resultData;
 		this.points = points;
 		initializeChart();
 		generatePoints();
-		
-		
-		//String[] dbl = new String;
+
+		// String[] dbl = new String;
 		addRegression();
-		
-		//if(callback.hasFinished())
-			return chart;
+
+		// if(callback.hasFinished())
+		return chart;
 	}
-  
-    public void initializeChart() {
-    	String info[]=resultData.get(resultData.size()-1); 
-        chart.setSize(1050,600)
-    	.setChartTitleText("Interpolation and Extrapolation "+info[2])
-        .setChartSubtitleText("Source: FAO. 2014. FAOSTAT. data.fao.org. (Accessed 1.9.2014)")
-        .setToolTip(new ToolTip()  
-                .setFormatter(  
-                    new ToolTipFormatter() {  
-                        public String format(ToolTipData toolTipData) {  
-                            return toolTipData.getXAsDouble() + ": " +  
-                                NumberFormat.getFormat("#,###").format(toolTipData.getYAsDouble());  
-                        }  
-                    }  
-                )  
-            )
-            .setAreaPlotOptions(new AreaPlotOptions()  
-                .setPointStart(1990)  
-                .setMarker(new Marker()  
-                    .setEnabled(false)  
-                    .setSymbol(Marker.Symbol.CIRCLE)  
-                    .setRadius(2)  
-                    .setHoverState(new Marker()  
-                        .setEnabled(true)  
-                    )  
-                )  
-  
-            );
 
-    chart.getXAxis()
-        .setCategories(  
-                "1990", "1991", "1992", "1993", "1994", "1995",  
-                "1996", "1997", "1998", "1999", "2000", "2001",
-                "2002", "2003", "2004", "2005", "2006", "2007",
-                "2008", "2009", "2010", "2011", "2012", "2013",
-                "2014", "2015", "2016"
-            ) 
-         .setTickInterval(5);
+	public void initializeChart() {
+		String info[] = resultData.get(resultData.size() - 1);
+		chart.setSize(1050, 600)
+				.setChartTitleText("Interpolation and Extrapolation " + info[2])
+				.setChartSubtitleText(
+						"Source: FAO. 2014. FAOSTAT. data.fao.org. (Accessed 1.9.2014)")
+				.setToolTip(new ToolTip().setFormatter(new ToolTipFormatter() {
+					public String format(ToolTipData toolTipData) {
+						return toolTipData.getXAsDouble()
+								+ ": "
+								+ NumberFormat.getFormat("#'###").format(
+										toolTipData.getYAsDouble());
+					}
+				}))
+				.setAreaPlotOptions(
+						new AreaPlotOptions().setPointStart(1990).setMarker(
+								new Marker()
+										.setEnabled(false)
+										.setSymbol(Marker.Symbol.CIRCLE)
+										.setRadius(2)
+										.setHoverState(
+												new Marker().setEnabled(true)))
 
-    final YAxis axis = chart.getYAxis();   
-    axis.setAxisTitleText("Value in tonnes")  
-        //.setMin(0)  
-        .setMinorGridLineWidth(0)  
-        .setGridLineWidth(0)  
-        .setAlternateGridColor(null);  
-   	
-    }
-    
-    private void generatePoints(){
-   		String [] temp2 = resultData.get(0);
-   		chart.addSeries(chart.createSeries()  
-   			.setName(temp2[1])
-   			.setType(Series.Type.SPLINE)
-   			.setPoints(new Number[]{  
-   				points[0], points[1], points[2], points[3], points[4], points[5],  
-   				points[6], points[7], points[8], points[9], points[10], points[11], 
-   				points[12], points[13], points[14], points[15], points[16], points[17], 
-   				points[18], points[19], points[20], points[21]/**/
-   			})
-   		);
-   		
-    }
-   		
-   		private void addRegression(){
-   			String [] temp2 = resultData.get(0);
-   			chart.addSeries(chart.createSeries()  
-   		            .setName("Regression Line "+temp2[1])  
-   		            .setType(Series.Type.SPLINE)  
-   		            .setPlotOptions(new LinePlotOptions()  
-   		                .setMarker(new Marker()  
-   		                    .setEnabled(true)  
-   		                )  
-   		                .setHoverStateLineWidth(0)  
-   		                .setEnableMouseTracking(true)  
-   		            ) 
-   		
-   			
-   		            .setPoints(new Number[][]{
-   		            		{0.0, resultReg[0]}, {26.0, resultReg[26]}
-   		                
-   		            })  
-   		         
-   		);
+				);
 
-   	}
+		chart.getXAxis()
+				.setCategories("1990", "1991", "1992", "1993", "1994", "1995",
+						"1996", "1997", "1998", "1999", "2000", "2001", "2002",
+						"2003", "2004", "2005", "2006", "2007", "2008", "2009",
+						"2010", "2011", "2012", "2013", "2014", "2015", "2016")
+				.setTickInterval(5);
+
+		final YAxis axis = chart.getYAxis();
+		axis.setAxisTitleText("Value in tonnes")
+				// .setMin(0)
+				.setMinorGridLineWidth(0).setGridLineWidth(0)
+				.setAlternateGridColor(null);
+
+	}
+
+	private void generatePoints() {
+		String[] temp2 = resultData.get(0);
+		chart.addSeries(chart
+				.createSeries()
+				.setName(temp2[1])
+				.setType(Series.Type.SPLINE)
+				.setPoints(
+						new Number[] { points[0], points[1], points[2],
+								points[3], points[4], points[5], points[6],
+								points[7], points[8], points[9], points[10],
+								points[11], points[12], points[13], points[14],
+								points[15], points[16], points[17], points[18],
+								points[19], points[20], points[21] /**/
+						}));
+
+	}
+
+	private void addRegression() {
+		String[] temp2 = resultData.get(0);
+		chart.addSeries(chart
+				.createSeries()
+				.setName("Regression Line " + temp2[1])
+				.setType(Series.Type.SPLINE)
+				.setPlotOptions(
+						new LinePlotOptions()
+								.setMarker(new Marker().setEnabled(true))
+								.setHoverStateLineWidth(0)
+								.setEnableMouseTracking(true))
+
+				.setPoints(
+						new Number[][] { { 0.0, resultReg[0] },
+								{ 26.0, resultReg[26] }
+
+						})
+
+		);
+
+	}
 }
