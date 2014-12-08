759b312e397be054b7d9b87661db0cf9607d1092
WilliamMartini <william.martini@outlook.com>
2014-12-08 15:32:04 +0100

    Wer die Wahrheit sagt, braucht ein schnelles Pferd.

--- AgrarAlpha_v1/src/com/gwt/client/CreateView.java
+++ AgrarAlpha_v1/src/com/gwt/client/CreateView.java
@@ -7,36 +7,23 @@ import java.util.logging.Logger;
 
 import org.moxieapps.gwt.highcharts.client.Chart;
 
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ButtonBase;
-import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
 import com.google.gwt.visualization.client.VisualizationUtils;
 import com.google.gwt.visualization.client.visualizations.GeoMap;
-import com.google.gwt.visualization.client.visualizations.Table;
 import com.google.gwt.widgetideas.client.SliderBar;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 
 public class CreateView extends Composite{
 	
@@ -57,10 +44,10 @@ public class CreateView extends Composite{
 	public static final Logger log = Logger.getLogger(CreateView.class.getName());
 	final VisualizationLineChart vLineChart = new VisualizationLineChart();
 	VisualizationBarChart vBarChart;
-//private VisualizationLineChart vLineChart;
 
 	/* This class present the view the user has after he clicked the create button on mainView. it contains the graphics the user wants to see
 	 */
+
 	public CreateView(boolean interpolation, final ArrayList<String[]> Data, final String year){
 		initWidget(this.basePanel);
 		this.dataArray = Data;
@@ -85,18 +72,13 @@ public class CreateView extends Composite{
 		VisualizationTable vTable = new VisualizationTable(Data);
 		list=new ListBox();
 		label= new Label("Placeholder");
-		/*
-		 for(int i=min; i<=max; i++){
-		 list.addItem(String.valueOf(i));
-		 }
-		 */
 		
 		rankingPanel = new VerticalPanel();
 		rankingPanel.add(new SourceView());
 		rankingPanel.add(slider.asWidget());
         vRanking= new VisualizationRanking(dataArray, year);
         HTML ranking=vRanking.create();
-		rankingPanel.add(vRanking.create());	
+		rankingPanel.add(ranking);	
         rankingPanel.setCellHorizontalAlignment(ranking,HasHorizontalAlignment.ALIGN_CENTER);
 		
         tablePanel = new VerticalPanel();
@@ -106,40 +88,18 @@ public class CreateView extends Composite{
 
 		mapPanel = new VerticalPanel();
 
-		
+		tablePanel.add(new SourceView());
+		tablePanel.add(vTable.create());
 
-		  
+		addLineChart();
 		
-		/*if(interpolation==true){*/
-		    tablePanel.add(new SourceView());
-			tablePanel.add(vTable.create());
-			
-			
-			
-			
-			addLineChart();
-		
-		/*}
-		if(interpolation==false){
-			tablePanel.add(vTable.create());
-			//interpolationPanel.add(vMap.createChart());
-		}
-		//graphPanel.add(source); // adding a verticalPanel with all source to the mapPanel
-		/*
-		Runnable onLoadCallbackMap = new Runnable(){
-			public void run(){
-				mapPanel.add(VisMan.graphs.get(1));
-			}
-		};
+		list = new ListBox();
+		list.addItem("bla");
 
-		VisualizationUtils.loadVisualizationApi(onLoadCallbackMap, GeoMap.PACKAGE);
-		  */
-		//mapPanel.add(message.asWidget());
-		
-		//mapPanel.add(vMap.createChart());
-		//vMap.createChart(mapPanel);
-		
-		
+		/**createBtn = new Button("Create");
+		createBtn.addClickHandler(new createClickHandler());
+		createBtn.addStyleName("beautifulbutton2");*/
+			
 		sliderHisto.setStepSize(1);
 		sliderHisto.setCurrentValue(Integer.parseInt(year));
 		sliderHisto.setNumTicks(21);
