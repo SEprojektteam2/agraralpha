235cd6a9c4f89de792ec747ff1f320998946280a
rpernischova <romana.pernischova@uzh.ch>
2014-12-08 23:17:53 +0100

    clean up

--- AgrarAlpha_v1/src/com/gwt/client/AgrarAlpha_v1.java
+++ AgrarAlpha_v1/src/com/gwt/client/AgrarAlpha_v1.java
@@ -1,31 +1,7 @@
 package com.gwt.client;
 
-import java.util.ArrayList;
-
-import org.moxieapps.gwt.highcharts.client.Chart;
-
-import com.gwt.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.FormPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
-import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
